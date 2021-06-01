05eaf6e04b776ec9dac02006889680c8bf3ac913
Jonas <jat.schreiner@gmail.com>
2021-06-01 20:54:04 +0200

    Demo Project

---
+++ app/src/main/java/com/example/livedemo/Item.kt
@@ -0,0 +1,5 @@
+package com.example.livedemo
+
+data class Item (
+
+        )
---
+++ app/src/main/java/com/example/livedemo/ListAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.livedemo
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter (
+    private val list: MutableList<Item>
+        ) : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+
+            class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+
+
+        }
